1cb1633eff5679d9e3d779fe71c07292b2b283b0
Juha Komulainen <juha.komulainen@evident.fi>
2015-05-21 16:11:41 +0300

    Dump basic structure of APIs when running apina

--- apina-spring/src/main/java/fi/evident/apina/spring/SpringModelReader.java
+++ apina-spring/src/main/java/fi/evident/apina/spring/SpringModelReader.java
@@ -1,12 +1,17 @@
 package fi.evident.apina.spring;
 
 import fi.evident.apina.model.ApiDefinition;
+import fi.evident.apina.model.Endpoint;
 import fi.evident.apina.model.EndpointGroup;
+import fi.evident.apina.model.URITemplate;
 import fi.evident.apina.spring.java.model.*;
 import fi.evident.apina.spring.java.reader.ClassMetadataCollectionLoader;
 import fi.evident.apina.spring.java.reader.Classpath;
 
 import java.io.IOException;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
 
 /**
  * Builds {@link ApiDefinition} by reading the classes of a Spring Web MVC application.
@@ -14,6 +19,7 @@ import java.io.IOException;
 public final class SpringModelReader {
 
     private static final QualifiedName REST_CONTROLLER = new QualifiedName("org.springframework.web.bind.annotation.RestController");
+    private static final QualifiedName REQUEST_MAPPING = new QualifiedName("org.springframework.web.bind.annotation.RequestMapping");
 
     private SpringModelReader() { }
 
@@ -30,27 +36,39 @@ public final class SpringModelReader {
     }
 
     private static EndpointGroup createEndpointGroupForController(JavaClass javaClass) {
-        javaClass.getAnnotations().forEach(System.out::println);
-        System.out.println("class " + javaClass.getName() + " {");
-        System.out.println();
+        EndpointGroup endpointGroup = new EndpointGroup(javaClass.getName().toString());
 
-        for (JavaField javaField : javaClass.getFields()) {
-            for (JavaAnnotation annotation : javaField.getAnnotations())
-                System.out.println("    " + annotation);
-            System.out.println("    " + javaField);
-            System.out.println();
+        for (JavaMethod method : javaClass.getMethods()) {
+            if (method.getVisibility() == JavaVisibility.PUBLIC && !method.isStatic() && method.hasAnnotation(REQUEST_MAPPING)) {
+                endpointGroup.addEndpoint(createEndpointForMethod(javaClass, method));
+            }
         }
 
-        for (JavaMethod javaMethod : javaClass.getMethods()) {
-            for (JavaAnnotation annotation : javaMethod.getAnnotations())
-                System.out.println("    " + annotation);
-            System.out.println("    " + javaMethod);
-            System.out.println();
-        }
+        return endpointGroup;
+    }
 
-        System.out.println("}");
-        System.out.println();
+    private static Endpoint createEndpointForMethod(JavaClass javaClass, JavaMethod method) {
+        return new Endpoint(method.getName(), resolveUriTemplate(javaClass, method));
+    }
 
-        return new EndpointGroup(javaClass.getName().toString());
+    private static URITemplate resolveUriTemplate(JavaClass javaClass, JavaMethod method) {
+        String classUrl = findRequestMappingPath(javaClass);
+        String methodUrl = findRequestMappingPath(method);
+        String springPathTemplate = classUrl + methodUrl;
+
+        // TODO: property parse spring-templates so that we can create a proper URITemplate
+
+        return new URITemplate(springPathTemplate);
+    }
+
+    private static String findRequestMappingPath(JavaAnnotatedElement element) {
+        List<Object> values = element.findAnnotation(REQUEST_MAPPING).map(a -> a.getAttributeValues("value")).orElse(emptyList());
+
+        if (values.isEmpty())
+            return "";
+        else if (values.size() == 1)
+            return (String) values.get(0);
+        else
+            throw new RuntimeException("@RequestMapping -annotations with multiple values are not supported");
     }
 }
--- apina-tsang/src/main/java/fi/evident/apina/tsang/AngularTypeScriptWriter.java
+++ apina-tsang/src/main/java/fi/evident/apina/tsang/AngularTypeScriptWriter.java
@@ -1,12 +1,19 @@
 package fi.evident.apina.tsang;
 
 import fi.evident.apina.model.ApiDefinition;
+import fi.evident.apina.model.Endpoint;
+import fi.evident.apina.model.EndpointGroup;
 
 import java.nio.file.Path;
 
 public final class AngularTypeScriptWriter {
 
     public static void writeModel(Path output, ApiDefinition api) {
-        throw new UnsupportedOperationException();
+        for (EndpointGroup endpointGroup : api.getEndpointGroups()) {
+            System.out.println(endpointGroup.getName());
+            for (Endpoint endpoint : endpointGroup.getEndpoints()) {
+                System.out.println("    " + endpoint.getName() + " " + endpoint.getUriTemplate());
+            }
+        }
     }
 }
