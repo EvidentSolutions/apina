6c115559ff160eb30a949f86c1dbba7bfbb6f4d4
Juha Komulainen <juha.komulainen@evident.fi>
2015-05-21 15:52:28 +0300

    Remove duplication in test code

--- apina-spring/src/test/java/fi/evident/apina/spring/java/model/JavaAnnotationTest.java
+++ apina-spring/src/test/java/fi/evident/apina/spring/java/model/JavaAnnotationTest.java
@@ -9,13 +9,13 @@ public class JavaAnnotationTest {
 
     @Test
     public void toStringWithoutParameters() {
-        JavaAnnotation annotation = new JavaAnnotation(new QualifiedName("foo.bar.Baz"));
+        JavaAnnotation annotation = newAnnotation("foo.bar.Baz");
         assertThat(annotation.toString(), is("@foo.bar.Baz"));
     }
 
     @Test
     public void toStringWithJustValue() {
-        JavaAnnotation annotation = new JavaAnnotation(new QualifiedName("foo.bar.Baz"));
+        JavaAnnotation annotation = newAnnotation("foo.bar.Baz");
         annotation.setAttribute("value", 42);
 
         assertThat(annotation.toString(), is("@foo.bar.Baz(42)"));
@@ -23,7 +23,7 @@ public class JavaAnnotationTest {
 
     @Test
     public void toStringWithNonValueAttribute() {
-        JavaAnnotation annotation = new JavaAnnotation(new QualifiedName("foo.bar.Baz"));
+        JavaAnnotation annotation = newAnnotation("foo.bar.Baz");
         annotation.setAttribute("foo", 42);
 
         assertThat(annotation.toString(), is("@foo.bar.Baz(foo=42)"));
@@ -31,11 +31,15 @@ public class JavaAnnotationTest {
 
     @Test
     public void toStringWithMultipleAttributes() {
-        JavaAnnotation annotation = new JavaAnnotation(new QualifiedName("foo.bar.Baz"));
+        JavaAnnotation annotation = newAnnotation("foo.bar.Baz");
         annotation.setAttribute("value", 1);
         annotation.setAttribute("foo", 2);
         annotation.setAttribute("bar", 3);
 
         assertThat(annotation.toString(), is("@foo.bar.Baz(value=1, foo=2, bar=3)"));
+    }
+
+    private static JavaAnnotation newAnnotation(String name) {
+        return new JavaAnnotation(new QualifiedName(name));
     }
 }
