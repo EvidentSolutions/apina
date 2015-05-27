af8a7a2799a7b99556bab5503db315d233731cb2
Juha Komulainen <juha.komulainen@evident.fi>
2015-05-27 07:03:03 +0300

    Add class for raw generated code fragments

---
+++ apina-core/src/main/java/fi/evident/apina/tsang/RawCode.java
@@ -0,0 +1,18 @@
+package fi.evident.apina.tsang;
+
+/**
+ * Represents raw piece of code written as it is when passed to {@link CodeWriter#writeValue(Object)}.
+ */
+final class RawCode {
+
+    private final Object value;
+
+    RawCode(Object value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+}
