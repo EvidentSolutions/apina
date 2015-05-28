4dd4dfa410b8add70fa34b4e28b9d3dc1b8f577b
Juha Komulainen <juha.komulainen@evident.fi>
2015-05-28 09:37:17 +0300

    Support generating code to specified output file
    Add an optional command line option for output file. If the output file
    is not given, then write to stdout like before.

--- apina-cli/src/main/java/fi/evident/apina/cli/Apina.java
+++ apina-cli/src/main/java/fi/evident/apina/cli/Apina.java
@@ -7,12 +7,16 @@ import fi.evident.apina.tsang.AngularTypeScriptWriter;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public final class Apina {
 
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.printf("usage: apina INPUT1%sINPUT2%s...\n", File.pathSeparator, File.pathSeparator);
+        if (args.length != 1 && args.length != 2) {
+            System.err.printf("usage: apina INPUT1%sINPUT2%s... [OUTPUT]\n", File.pathSeparator, File.pathSeparator);
             System.exit(1);
         }
 
@@ -31,7 +35,13 @@ public final class Apina {
 
             writer.writeApi();
 
-            System.out.println(writer.getOutput());
+            String output = writer.getOutput();
+            if (args.length == 2) {
+                Path outputFile = Paths.get(args[1]);
+                Files.write(outputFile, output.getBytes(StandardCharsets.UTF_8));
+            } else {
+                System.out.println(output);
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
