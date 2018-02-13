986b7bdecc00e0acd0348ae6d9b38f6e37ed4a93
Juha Komulainen <juha.komulainen@evident.fi>
2018-02-13 11:52:31 +0200

    Don't add null parameters to requests

--- apina-core/src/main/resources/typescript/runtime-angular2.ts
+++ apina-core/src/main/resources/typescript/runtime-angular2.ts
@@ -163,8 +163,17 @@ export class DefaultApinaEndpointContext extends ApinaEndpointContext {
 
         const requestParams = data.requestParams;
         let params: HttpParams | undefined = undefined;
-        if (requestParams != null)
-            params = new HttpParams({fromObject: requestParams});
+        if (requestParams != null) {
+            const filteredParams = {};
+            for (const key of Object.keys(requestParams)) {
+                const value = requestParams[key];
+                if (value != null)
+                    filteredParams[key] = value;
+            }
+
+            params = new HttpParams({fromObject: filteredParams});
+        }
+
 
         return this.httpClient.request(data.method, url, { params: params, body: data.requestBody })
             .pipe(map(r => data.responseType ? this.config.deserialize(r, data.responseType) : r));
