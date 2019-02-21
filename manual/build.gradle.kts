8ba113c945baaa36d67683827bf0c56972f85208
Juha Komulainen <juha.komulainen@evident.fi>
2019-02-21 19:00:22 +0200

    Add task for deploying manual

--- manual/build.gradle.kts
+++ manual/build.gradle.kts
@@ -1,8 +1,23 @@
 plugins {
     id("org.asciidoctor.convert") version "1.5.9.2"
+    id("org.ajoberstar.github-pages") version "1.6.0"
 }
 
 tasks.asciidoctor {
     sourceDir = file("src/asciidoc")
     attributes = mapOf("revnumber" to project.version.toString())
+}
+
+tasks.publishGhPages {
+    dependsOn(tasks.asciidoctor)
+}
+
+githubPages {
+    setRepoUri("git@github.com:EvidentSolutions/apina.git")
+    deleteExistingFiles = true
+
+    pages(closureOf<CopySpec> {
+        from(files("src/CNAME"))
+        from(files("build/asciidoc/html5"))
+    })
 }
