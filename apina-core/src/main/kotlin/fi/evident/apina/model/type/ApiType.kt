03283683b7f43c02793f16c41cacca508b002641
Juha Komulainen <juha.komulainen@evident.fi>
2019-03-04 22:43:30 +0200

    Implement ApiTypes as data classes

--- apina-core/src/main/kotlin/fi/evident/apina/model/type/ApiType.kt
+++ apina-core/src/main/kotlin/fi/evident/apina/model/type/ApiType.kt
@@ -1,7 +1,5 @@
 package fi.evident.apina.model.type
 
-import java.util.*
-
 sealed class ApiType {
 
     abstract fun typeRepresentation(): String
@@ -11,41 +9,31 @@ sealed class ApiType {
     abstract override fun equals(other: Any?): Boolean
     abstract override fun hashCode(): Int
 
-    class Array(val elementType: ApiType) : ApiType() {
+    data class Array(val elementType: ApiType) : ApiType() {
         override fun typeRepresentation() = "$elementType[]"
-        override fun equals(other: Any?) = other is Array && elementType == other.elementType
-        override fun hashCode() = elementType.hashCode()
     }
 
-    class BlackBox(val name: ApiTypeName) : ApiType() {
+    data class BlackBox(val name: ApiTypeName) : ApiType() {
         override fun typeRepresentation() = name.toString()
-        override fun equals(other: Any?) = other is BlackBox && name == other.name
-        override fun hashCode() = name.hashCode()
     }
 
     /**
      * Represents class types.
      */
-    class Class(val name: ApiTypeName) : ApiType(), Comparable<Class> {
+    data class Class(val name: ApiTypeName) : ApiType(), Comparable<Class> {
 
         constructor(name: String): this(ApiTypeName(name))
         override fun typeRepresentation() = name.toString()
-        override fun equals(other: Any?) = other is Class && name == other.name
-        override fun hashCode() = name.hashCode()
         override fun compareTo(other: Class) = name.compareTo(other.name)
     }
 
-    class Dictionary(private val valueType: ApiType) : ApiType() {
+    data class Dictionary(private val valueType: ApiType) : ApiType() {
         override fun typeRepresentation() = "Dictionary<$valueType>"
-        override fun equals(other: Any?) = other is Dictionary && valueType == other.valueType
-        override fun hashCode() = Objects.hash(valueType)
     }
 
-    class Nullable(val type: ApiType) : ApiType() {
+    data class Nullable(val type: ApiType) : ApiType() {
         override fun typeRepresentation() = type.typeRepresentation() + " | null"
         override fun unwrapNullable() = type
-        override fun equals(other: Any?) = other is Nullable && type == other.type
-        override fun hashCode() = Objects.hash(type)
     }
 
     class Primitive private constructor(private val name: String) : ApiType() {
