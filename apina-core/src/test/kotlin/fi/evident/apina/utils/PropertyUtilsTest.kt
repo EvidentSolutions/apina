128f62fb21388de281b8c043ca53e31b0f97c0e3
Tuomo Ala-Vannesluoma <tuomo.ala-vannesluoma@solita.fi>
2021-10-05 10:51:25 +0300

    Add valueclass to kotlinx translator test and add value class test to property utils

--- apina-core/src/test/kotlin/fi/evident/apina/utils/PropertyUtilsTest.kt
+++ apina-core/src/test/kotlin/fi/evident/apina/utils/PropertyUtilsTest.kt
@@ -6,6 +6,14 @@ import kotlin.test.assertEquals
 class PropertyUtilsTest {
 
     @Test
+    fun propertyNamesForValueGetters() {
+        assertEquals("foo", propertyNameForGetter("getFoo-11MJ8YI"))
+        assertEquals("fooBar", propertyNameForGetter("getFooBar-11MJ8YI"))
+        assertEquals("foo", propertyNameForGetter("isFoo-11MJ8YI"))
+        assertEquals("fooBar", propertyNameForGetter("isFooBar-11MJ8YI"))
+    }
+
+    @Test
     fun propertyNamesForGetters() {
         assertEquals("foo", propertyNameForGetter("getFoo"))
         assertEquals("fooBar", propertyNameForGetter("getFooBar"))
