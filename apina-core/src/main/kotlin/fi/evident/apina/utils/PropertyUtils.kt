15551d12485cc2453e9140fd6fd2a4f5fa7efca0
Tuomo Ala-Vannesluoma <tuomo.ala-vannesluoma@solita.fi>
2021-10-05 08:40:02 +0300

    Add support for value classes

--- apina-core/src/main/kotlin/fi/evident/apina/utils/PropertyUtils.kt
+++ apina-core/src/main/kotlin/fi/evident/apina/utils/PropertyUtils.kt
@@ -1,8 +1,19 @@
 package fi.evident.apina.utils
 
+import java.util.Locale
+
 fun propertyNameForGetter(getterName: String): String =
     when {
-        getterName.startsWith("get") -> getterName.removePrefix("get").decapitalize()
-        getterName.startsWith("is") -> getterName.removePrefix("is").decapitalize()
+        // Value/inline classes result in name suffixed with hash
+        getterName.contains("-") -> propertyNameForSimpleGetter(getterName.substringBefore('-'))
+        else -> propertyNameForSimpleGetter(getterName)
+    }
+
+private fun propertyNameForSimpleGetter(getterName: String): String =
+    when {
+        getterName.startsWith("get") -> getterName.removePrefix("get")
+            .replaceFirstChar { it.lowercase(Locale.getDefault()) }
+        getterName.startsWith("is") -> getterName.removePrefix("is")
+            .replaceFirstChar { it.lowercase(Locale.getDefault()) }
         else -> error("not a valid name for getter $getterName")
     }
