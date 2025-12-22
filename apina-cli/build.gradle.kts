ddcfe07f7a125c31c8f9f7e462727f8d05560dda
Juha Komulainen <juha.komulainen@evident.fi>
2025-12-22 13:01:27 +0200

    Migrate to Vanniktech Maven Publish Plugin

--- apina-cli/build.gradle.kts
+++ apina-cli/build.gradle.kts
@@ -3,8 +3,7 @@ import java.net.URI
 plugins {
     application
     kotlin("jvm")
-    `maven-publish`
-    signing
+    alias(libs.plugins.vanniktech.publish)
     alias(libs.plugins.shadow)
 }
 
@@ -19,89 +18,63 @@ application {
     mainClass.set("fi.evident.apina.cli.Apina")
 }
 
-val sourcesJar = task<Jar>("sourcesJar") {
-    dependsOn("classes")
-    archiveClassifier.set("sources")
-
-    from(sourceSets.main.get().allSource)
-}
-
-val javadoc: Javadoc by tasks
-
-val javadocJar = task<Jar>("javadocJar") {
-    dependsOn(javadoc)
-    archiveClassifier.set("javadoc")
-    from(javadoc.destinationDir)
-}
-
 tasks.shadowJar.configure {
     archiveClassifier.set("")
+}
+
+tasks.jar {
+    archiveClassifier.set("plain")
 }
 
 tasks.startScripts.configure {
     dependsOn(tasks.shadowJar)
 }
 
-tasks.jar {
-    enabled = false
+val sourcesJar by tasks.registering(Jar::class) {
+    dependsOn("classes")
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
 }
 
-artifacts.add(configurations.archives.name, tasks.shadowJar)
-artifacts.add(configurations.archives.name, sourcesJar)
-artifacts.add(configurations.archives.name, javadocJar)
-
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            project.shadow.component(this)
-            artifact(sourcesJar)
-            artifact(javadocJar)
-
-            pom {
-                name.set("apina-cli")
-                description.set("Tool for creating TypeScript client code from Spring controllers and Jackson classes")
-                url.set("https://github.com/evidentsolutions/apina")
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://opensource.org/licenses/MIT")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("komu")
-                        name.set("Juha Komulainen")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git@github.com:evidentsolutions/apina.git")
-                    developerConnection.set("scm:git:git@github.com:evidentsolutions/apina.git")
-                    url.set("https://github.com/evidentsolutions/apina")
-                }
-            }
-        }
-    }
-
-    if (hasProperty("sonatypeUsername")) {
-        repositories {
-            maven {
-                name = "sonatype"
-
-                url = if (version.toString().endsWith("-SNAPSHOT"))
-                    URI("https://oss.sonatype.org/content/repositories/snapshots/")
-                else
-                    URI("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-
-                credentials {
-                    username = property("sonatypeUsername") as String
-                    password = property("sonatypePassword") as String
-                }
-            }
-        }
-    }
+val javadocJar by tasks.registering(Jar::class) {
+    dependsOn(tasks.javadoc)
+    archiveClassifier.set("javadoc")
+    from(tasks.javadoc.get().destinationDir)
 }
 
-signing {
-    sign(publishing.publications["mavenJava"])
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
+
+    coordinates(
+        groupId = "fi.evident.apina",
+        artifactId = "apina-cli",
+        version = project.version as String
+    )
+
+    pom {
+        name.set("apina-cli")
+        description.set("Tool for creating TypeScript client code from Spring controllers and Jackson classes")
+        url.set("https://github.com/evidentsolutions/apina")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("komu")
+                name.set("Juha Komulainen")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:git@github.com:evidentsolutions/apina.git")
+            developerConnection.set("scm:git:git@github.com:evidentsolutions/apina.git")
+            url.set("https://github.com/evidentsolutions/apina")
+        }
+    }
 }
--- apina-core/build.gradle.kts
+++ apina-core/build.gradle.kts
@@ -4,8 +4,7 @@ import java.net.URI
 plugins {
     java
     alias(libs.plugins.kotlin.jvm)
-    `maven-publish`
-    signing
+    alias(libs.plugins.vanniktech.publish)
     alias(libs.plugins.shadow)
 }
 
@@ -60,80 +59,55 @@ tasks.shadowJar {
     }
 }
 
-val sourcesJar = task<Jar>("sourcesJar") {
+tasks.jar {
+    archiveClassifier.set("plain")
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
     dependsOn("classes")
     archiveClassifier.set("sources")
-
     from(sourceSets.main.get().allSource)
 }
 
-val javadoc: Javadoc by tasks
-
-val javadocJar = task<Jar>("javadocJar") {
-    dependsOn(javadoc)
+val javadocJar by tasks.registering(Jar::class) {
+    dependsOn(tasks.javadoc)
     archiveClassifier.set("javadoc")
-    from(javadoc.destinationDir)
+    from(tasks.javadoc.get().destinationDir)
 }
 
-tasks.jar {
-    enabled = false
-}
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
 
-artifacts.add(configurations.archives.name, tasks.shadowJar)
-artifacts.add(configurations.archives.name, sourcesJar)
-artifacts.add(configurations.archives.name, javadocJar)
+    coordinates(
+        groupId = "fi.evident.apina",
+        artifactId = "apina-core",
+        version = project.version as String
+    )
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            project.shadow.component(this)
-            artifact(sourcesJar)
-            artifact(javadocJar)
+    pom {
+        name.set("apina-core")
+        description.set("Tool for creating TypeScript client code from Spring controllers and Jackson classes")
+        url.set("https://github.com/evidentsolutions/apina")
 
-            pom {
-                name.set("apina-core")
-                description.set("Tool for creating TypeScript client code from Spring controllers and Jackson classes")
-                url.set("https://github.com/evidentsolutions/apina")
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://opensource.org/licenses/MIT")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("komu")
-                        name.set("Juha Komulainen")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git@github.com:evidentsolutions/apina.git")
-                    developerConnection.set("scm:git:git@github.com:evidentsolutions/apina.git")
-                    url.set("https://github.com/evidentsolutions/apina")
-                }
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/licenses/MIT")
             }
         }
-    }
 
-    if (hasProperty("sonatypeUsername")) {
-        repositories {
-            maven {
-                name = "sonatype"
-
-                url = if (version.toString().endsWith("-SNAPSHOT"))
-                    URI("https://oss.sonatype.org/content/repositories/snapshots/")
-                else
-                    URI("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-
-                credentials {
-                    username = property("sonatypeUsername") as String
-                    password = property("sonatypePassword") as String
-                }
+        developers {
+            developer {
+                id.set("komu")
+                name.set("Juha Komulainen")
             }
         }
-    }
-}
 
-signing {
-    sign(publishing.publications["mavenJava"])
+        scm {
+            connection.set("scm:git:git@github.com:evidentsolutions/apina.git")
+            developerConnection.set("scm:git:git@github.com:evidentsolutions/apina.git")
+            url.set("https://github.com/evidentsolutions/apina")
+        }
+    }
 }
