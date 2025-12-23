21d2bec5e79558ed6cb1bf778db0f71466f792b5
Juha Komulainen <juha.komulainen@evident.fi>
2025-12-23 11:41:17 +0200

    Use GitHub for releases

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Include something like the following in your web application project:
 
 ```kotlin
 plugins {
-    id("fi.evident.apina") version "0.25.2"
+    id("fi.evident.apina") version "<version>"
 }
 
 tasks.apina {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,13 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import pl.allegro.tech.build.axion.release.domain.TagNameSerializationConfig
-import pl.allegro.tech.build.axion.release.domain.VersionConfig
 
 plugins {
-    alias(libs.plugins.axion.release)
     alias(libs.plugins.kotlin.jvm) apply false
-}
-
-configure<VersionConfig> {
-    tag(closureOf<TagNameSerializationConfig> {
-        prefix = "v"
-        versionSeparator = ""
-    })
 }
 
 configure(allprojects) {
     group = "fi.evident.apina"
-    this.version = rootProject.scmVersion.version
+    version = project.findProperty("projectVersion") as String? ?: "0.1.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
